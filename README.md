9308311b045fb36134152609d90bcfc61ddc08ff
Oliver Binns <mail@oliverbinns.co.uk>
2016-08-01 17:03:58 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Vanbrugh-College-Website" 
---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Vanbrugh College - Home</title>
+		<!--Import Google Icon Font-->
+		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import materialize.css-->
+		<!--link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css"  media="screen,projection"/-->
+		<link type="text/css" rel="stylesheet" href="sass/ghpages-materialize.css"  media="screen,projection"/>
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+	<body>
+		<header>
+			<nav class="top-nav">
+				<div class="container">
+					<div class="nav-wrapper">
+						<a class="page-title">Vanbrugh College JCR</a>
+					</div>
+				</div>
+			</nav>
+		</header>
+		<main>
+			<div class="row">
+				<div class="col s6 m3">
+					<div class="center promo promo-example">
+						<i class="material-icons">web</i>
+						<p class="promo-caption">College Website</p>
+						<p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+					</div>
+				</div>
+				<div class="col s6 m3">
+					<div class="center promo promo-example">
+						<i class="material-icons">flash_on</i>
+						<p class="promo-caption">Twitter</p>
+						<p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+					</div>
+				</div>
+				<div class="col s6 m3">
+					<div class="center promo promo-example">
+						<i class="material-icons">flash_on</i>
+						<p class="promo-caption">Facebook</p>
+						<p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+					</div>
+				</div>
+				<div class="col s6 m3">
+					<div class="center promo promo-example">
+						<i class="material-icons">flash_on</i>
+						<p class="promo-caption">Snapchat</p>
+						<p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+					</div>
+				</div>
+			
+			</div>
+		</main>
+		<footer class="page-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col l6 s12">
+						<h5 class="white-text">Vanbrugh College JCR</h5>
+						<p class="grey-text text-lighten-4">
+							Vanbrugh Way,<br>
+							University of York,<br>
+							York,<br>
+							YO10 5DD,<br>
+							United Kingdom
+						</p>
+					</div>
+					<div class="col l4 offset-l2 s12">
+						<h5 class="white-text">Links</h5>
+						<ul>
+							<li><a class="grey-text text-lighten-3" href="https://www.facebook.com/vanbrughcollege/" target="_blank">Facebook</a></li>
+							<li><a class="grey-text text-lighten-3" href="https://www.instagram.com/vanbrughcollege/" target="_blank">Instagram</a></li>
+							<li><a class="grey-text text-lighten-3" href="https://www.twitter.com/vanbrughjcr" target="_blank">Twitter</a></li>
+							<li><a class="grey-text text-lighten-3" href="https://www.yorkcolleges.co.uk/" target="_blank">York Colleges</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright">
+				<div class="container">
+					&copy; 2016, Vanbrugh College
+				</div>
+			</div>
+        </footer>
+	
+	
+		<!--Import jQuery before materialize.js-->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"/>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"/>
+	</body>
+</html>
